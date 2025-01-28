8ae5131161b9749cbe5f01bea65351c8397a217d
Ruvim1100 <ruvim-bozaji@mail.ru>
2025-01-28 16:25:04 +0200

    feat: configure DI, mapping, and CORS settings
    - Added necessary files and configured dependency injection.
    - Set up mapping using Automapper.
    - Configured CORS to enable proper API communication.
    - Preparation completed; the next step is to implement controllers.

--- Braimp.WebApi/Program.cs
+++ Braimp.WebApi/Program.cs
@@ -1,6 +1,46 @@
+using Braimp.Application;
+using Braimp.Application.Common.Mapping;
+using Braimp.Application.Interfaces;
+using Braimp.Persistence;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAutoMapper(configuration =>
+    {
+        configuration.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+        configuration.AddProfile(new AssemblyMappingProfile(typeof(IBraimpDbContext).Assembly));
+    });
+
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddCors(options =>
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowAnyOrigin()));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+
+    try
+    {
+        var context = serviceProvider.GetRequiredService<BraimpDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred during database initialization: {ex.Message}");
+        throw;
+    }
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+
+app.MapControllers();
 
 app.Run();
