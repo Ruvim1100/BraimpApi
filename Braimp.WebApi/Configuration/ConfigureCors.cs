e8ff2a3aecf650377f9048ac36f90deb54ed04d0
Ruvim1100 <ruvim-bozaji@mail.ru>
2025-07-10 18:45:26 +0300

    Ш фь ешкув

--- Braimp.WebApi/Configuration/ConfigureCors.cs
+++ Braimp.WebApi/Configuration/ConfigureCors.cs
@@ -3,14 +3,20 @@ public static class ConfigureCors
 {
     public static IServiceCollection AddBraimpCors(this IServiceCollection services, IConfiguration configuration)
     {
-        var allowedOrigins = configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+
+        var origin = configuration["Cors:AllowedOrigin"];
+
+        if (string.IsNullOrWhiteSpace(origin))
+        {
+            throw new InvalidOperationException("CORS origin not configured.");
+        }
 
         services.AddCors(options =>
         {
             options.AddPolicy("AllowFrontend", policy =>
             {
                 policy
-                    .WithOrigins("https://braimp-frontend-had8cgbjb7fxctev.westeurope-01.azurewebsites.net")
+                    .WithOrigins(origin)
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials();
